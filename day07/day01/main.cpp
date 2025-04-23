a44df3b2fc985969987ad89917639da851029e1b
PAVITHRA786786 <PAVITHRA124CHENNAVEERA@GMAIL.COM>
2025-04-23 10:23:58 +0530

    Add files via upload

---
+++ day07/day01/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <type.h>
+using namespace std;
+
+int main()
+{
+	typeconvimp();
+	typeconvexp();
+	//int a = b = c = 10;
+//	a = b = c = 50;
+	//cout << a << b << c << endl;
+	//int x[] = { 10,20,30,40,50 };
+//	cout << x[4] << 3[x] << x[2] << 1[x] << x[0] << endl;
+	/*int i;
+	i = 0x10 + 010 + 10;
+	cout << x = % x << i << endl;*/
+	return 0;
+}
---
+++ day07/day01/type.h
@@ -0,0 +1,4 @@
+#pragma once
+
+void typeconvimp();
+void typeconvexp();
